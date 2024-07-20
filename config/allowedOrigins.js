708f8b9cbeb7450697cb0d7bf7d4b91386de35d9
YashSrivastava2004 <155613967+YashSrivastava2004@users.noreply.github.com>
2024-07-20 21:49:50 +0530

    Add files via upload

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:4200'
-
+    'http://localhost:4200',
+    'https://sparkling-longma-d7a95a.netlify.app'
 ];
 
 module.exports = allowedOrigins;
